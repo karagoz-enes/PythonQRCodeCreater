063e1e0ddbccb6a941fb122841d071bb6f427fe9
karagoz-enes <99972121+karagoz-enes@users.noreply.github.com>
2023-02-04 21:42:46 +0300

    Add files via upload

---
+++ PythonQRCodeCreater/main.py
@@ -0,0 +1,9 @@
+import qrcode
+code = qrcode.QRCode(version=1,
+                     error_correction= qrcode.constants.ERROR_CORRECT_L,
+                     box_size=120,
+                     border=6)
+code.add_data("neganwashere")
+code.make(fit=True)
+image = code.make_image(fill_color=(12,123,66),back_color="white")
+image.save("qrcode.png")
